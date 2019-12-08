cdddc0d323d76c2fc7c6655cb8653481dc423c36
Jared S <jaredjames2020@gmail.com>
2019-12-08 17:30:12 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,17 +2,18 @@
 class Pirate
 
   attr_accessor :name, :weight, :height
-  PIRATES = []
+  
+  @all = []
 
   def initialize(params)
     @name = params[:name]
     @weight = params[:weight]
     @height = params[:height]
-    PIRATES << self 
+    @all << self 
   end 
 
   def self.all 
-    PIRATES
+   @all
   end 
 
 end
